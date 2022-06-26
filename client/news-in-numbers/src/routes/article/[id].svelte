86ebb6660121c1d9680287f60eba4385bf34c1ed
EiseZ <eise.zimmerman@outlook.com>
2022-06-26 11:33:54 +0200

    Able to get 1 article page on client

---
+++ client/news-in-numbers/src/routes/article/[id].svelte
@@ -0,0 +1,43 @@
+<script context="module">
+    export async function load({ params }) {
+        return {
+            props: {
+                articleId: params.id,
+            }
+        }
+    }
+</script>
+
+<script>
+    import { onMount } from "svelte";
+    let article;
+    let articleFound = false;
+    export let articleId;
+    onMount(async () => {
+        fetch(`http://localhost:4000/article/${articleId}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data) {
+                articleFound = true;
+            }
+            console.log(data.title);
+            article = data;
+        }).catch(error => {
+            console.log(error);
+            return [];
+        });
+    });
+
+    import NavBar from "../../components/NavBar.svelte"
+    import Article from "../../components/Article.svelte"
+
+    let navbarPages = ["home", "articles", "contact"];
+    let navbarHrefs = ["home", "articles", "contact"];
+</script>
+
+<NavBar name="News In Numbers" elements={navbarPages} hrefElements={navbarHrefs} />
+{#if articleFound}
+    <Article imgSrc={""} title={article.title} text={article.content} />
+{:else}
+    <!--TODO: Article not found-->
+{/if}
