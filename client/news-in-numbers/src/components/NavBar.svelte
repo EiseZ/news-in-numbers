1329d70e114fbf6df0dfac32aa10ad6bbb051091
Eise Zimmerman <eise.zimmerman@outlook.com>
2022-06-27 11:24:31 +0200

    Signup is working

--- client/news-in-numbers/src/components/NavBar.svelte
+++ client/news-in-numbers/src/components/NavBar.svelte
@@ -18,7 +18,7 @@
         </div>
         <div>
             <a href="login" class="inline-block text-sm px-4 py-2 leading-none text-white hover:text-grey mt-4 lg:mt-0">Login</a>
-            <a href="sing-up" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white mt-4 lg:mt-0">Sign Up</a>
+            <a href="signup" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white mt-4 lg:mt-0">Sign Up</a>
         </div>
   </div>
 </nav>
